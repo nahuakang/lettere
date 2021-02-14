92991f73e82928145cdadd74ba022c7354dc646e
nahuakang <kangnahua@gmail.com>
2021-02-14 01:54:09 +0100

    Refactor to use tcp listener for choosing ports randomly at test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 //! main.rs
 
 use lettere::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,15 +7,16 @@
 // under `[dev-dependencies]` in Cargo.toml.
 //
 // You can inspect what code gets generated using `cargo expand --test health_check`.
+use std::net::TcpListener;
 
 #[actix_rt::test]
 async fn health_check_test() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("failed to execute health check request");
@@ -24,7 +25,12 @@ async fn health_check_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = lettere::run().expect("failed to bind address");
+fn spawn_app() -> String {
+    // Binding to port 0 would trigger the OS to scan for an available port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = lettere::run(listener).expect("failed to bind address");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
